adbc34b93730f0a91af76e4258f2b93c3f4a0533
gonzalourielbonelli <gonzalourielbonelli@tradenethub.com>
2025-04-15 13:07:35 -0300

    02: customizaciones borders en inputs, botones, checkbox, text area y progress bar, cambios en fuente y radios

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,27 @@
+import { Button } from "@/components/ui/button";
+import { Checkbox } from "@/components/ui/checkbox";
+import { Input } from "@/components/ui/input";
+import { Progress } from "@/components/ui/progress";
+import { Textarea } from "@/components/ui/textarea";
 
 
 export default function Home() {
   return (
-    <div >
-      <p className="text-4xl font-bold">
-        Encontralo
-      </p>
+    <div className="flex flex-col gap-y-4 p-4">
+      <div>
+        <Button variant={"elevated"}>Soy un boton</Button>
+      </div>
+      <div>
+        <Input placeholder="Soy un input" />
+      </div>
+      <div>
+        <Progress value={50} max={100} />
+      </div>
+      <div>
+        <Textarea placeholder="Soy una text-area" />
+      </div><div>
+        <Checkbox />
+      </div>
     </div>
   );
 }
